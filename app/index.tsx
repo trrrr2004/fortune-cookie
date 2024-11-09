d556ef3c0fbfb607c77eeb28af001935a616c954
Polina <natochiy.p@gmail.com>
2024-11-10 00:05:42 +0300

    init

--- app/index.tsx
+++ app/index.tsx
@@ -46,7 +46,7 @@ const App = () => {
 
             {/* Изменили путь к изображению на локальный */}
             <Image
-                source={require('C:\\Users\\hisma\\git\\magic-ball\\assets\\images\\cookie.png')}  // Локальный путь к изображению
+                source={require('C:\\Users\\Admin\\Downloads\\git\\fortune-cookies\\assets\\images\\cookie.png')}  // Локальный путь к изображению
                 style={styles.cookieImage}
             />
 
